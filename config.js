383daef3db2e3bbbc09d0c0fe26f5e3ec403d06c
lythihuyen <116254992+lythihuyen@users.noreply.github.com>
2022-10-20 14:16:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'chào cậu!',
+    introDesc: `bọn tớ hân hạnh mới cậu đến dẩy đầm cùng chúng tớ!!!`,
+    btnIntro: 'zee',
+    title: 'nhất định cậu phải đi đó, bọn tớ chỉ có chờ mình cậu mà thôi <3',
+    desc: 'cậu đi nha? ',
+    btnYes: 'tất nhiên rồiii',
+    btnNo: 'hông đâu:3',
+    question:'gì cậu muốn nói gì cho bọn tớ á?',
+    btnReply: 'Gửi đii <3',
+    reply: 'các cậu số 1, 100 điểm <33333333',
+    mess: 'biết mà 🥰. nhớ đến nhenn 😘😘',
+    messDesc: '7h30 tối thứ 7 tuần này nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.youtube.com/watch?v=tOZoAsJO00w'
 }
